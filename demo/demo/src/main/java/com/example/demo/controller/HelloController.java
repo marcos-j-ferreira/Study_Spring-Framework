abb491469e38b491261939229708bcb71439e57d
marcos-j-ferreira <marcos.j.lemes.2004@gmail.com>
2025-07-07 12:28:36 -0300

    rota hello

--- demo/demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.exemplo.demo.controller;
+package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -6,12 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping("/api/v1/estudo")
-public class HelloController{
+public class HelloController {
 
-    @GetMapping("")
     @GetMapping("/hello")
-    public final String HelloWorld(){
-        return "Hello, world!!";
+    public String hello(){
+        return new String("olá, Seja muito bem-vindo!!");
+    }
 
-    } 
 }
